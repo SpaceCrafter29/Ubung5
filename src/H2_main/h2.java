c4853aebe747f98415b39d2a6fd403e2b44613b2
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-15 18:57:15 +0100

    h2 bearbeitet

--- src/H2_main/h2.java
+++ src/H2_main/h2.java
@@ -1,4 +1,19 @@
 package H2_main;
 
+import java.util.Arrays;
+
 public class h2 {
+    public static void main(String[] args){
+        int n = 234351;
+        int digit = 0;
+        int[] a = {1,2,3,4,5,6,7,8,9};
+        int j = 0;
+        for(int i=1;i<10;i++){
+            a[a.length-i]= (int) ((int) n % Math.pow(10,i) - j);
+            a[a.length-i] /= Math.pow(10,i-1);
+            j = (int) ((int) n % Math.pow(10,i));
+        }
+        System.out.println(Arrays.toString(a));
+
+    }
 }
