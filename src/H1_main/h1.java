891e3ec15d920a1367275ab29ecac865c291ad55
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-15 18:32:14 +0100

    h1 bearbeitet

--- src/H1_main/h1.java
+++ src/H1_main/h1.java
@@ -1,4 +1,17 @@
 package H1_main;
 
+import java.util.Arrays;
+
 public class h1 {
+    public static void main(String[] args){
+        int[] myArray = {1, 2, 3, 4, 5};
+        int test = 0;
+        int length = myArray.length;
+        for(int i = 0; i < (length / 2); i++){
+            int temp = myArray[i];
+            myArray[i] = myArray[length-i-1];
+            myArray[length-i-1] = temp ;
+        }
+        System.out.println(Arrays.toString(myArray));
+    }
 }
