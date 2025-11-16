4e76ed4f20f0e67eb3955d36fce95166d2d1e306
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-16 10:23:41 +0100

    updated p1

--- src/P1_main/p1.java
+++ src/P1_main/p1.java
@@ -1,4 +1,22 @@
 package P1_main;
 
 public class p1 {
+    public static void main(String[] args){
+        int x=0;
+        while ( x <= 10 ) {
+            x = x + 1;
+            System.out.println(x);
+        }
+        //a
+        for(int i = 1; i <= 11;i++){
+            System.out.println(i);
+        }
+        //b
+        x = 0;
+        do{
+            x++;
+            System.out.println(x);
+        }
+        while (x <= 10);;
+    }
 }
