451c21a245e42d388b4724e79f9bb303f4c24c32
SpaceCrafter29 <stefan.i.dobrea@gmail.com>
2025-11-18 16:19:26 +0100

    p2 bearbeitet

--- src/P2_main/p2.java
+++ src/P2_main/p2.java
@@ -1,4 +1,17 @@
 package P2_main;
 
 public class p2 {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        int max = 1;
+        int minIndex = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+                minIndex = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(minIndex);
+    }
 }
